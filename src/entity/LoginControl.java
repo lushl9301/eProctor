230d1445f551fe09133b8b881790fd1b5c820283
Gong Yue <apollox1961@outlook.com>
2014-02-26 05:00:31 +0800

    added password MD5

--- src/entity/LoginControl.java
+++ src/entity/LoginControl.java
@@ -1,11 +1,16 @@
 package entity;
 
 import java.awt.EventQueue;
+import java.awt.Window;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 import javax.swing.UIManager;
 
 public class LoginControl {
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	public LoginControl() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -23,5 +28,26 @@ public class LoginControl {
 	public static boolean verifyUser() {
 		Random rand = new Random();
 		return rand.nextBoolean();
+	}
+	
+	public static String getMD5(char[] cInput) throws NoSuchAlgorithmException {
+		String sInput = String.valueOf(cInput);
+		MessageDigest md;
+		byte[] bytesOfMessage = sInput.getBytes(StandardCharsets.UTF_8);
+		md = MessageDigest.getInstance("MD5");
+		byte[] thedigest = md.digest(bytesOfMessage);
+		String output = new String(thedigest, StandardCharsets.UTF_8);
+		output = LoginControl.bytesToHex(thedigest);
+		return output;
+	}
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
 	}
 }
