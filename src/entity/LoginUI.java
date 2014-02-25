d1f96290b7d1146608aa1ead94385847e5317553
Gong Yue <apollox1961@outlook.com>
2014-02-25 21:14:27 +0800

    LoginUI

--- src/entity/LoginUI.java
+++ src/entity/LoginUI.java
@@ -3,10 +3,18 @@ package entity;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.UIManager;
 
 public class LoginUI {
 
 	private JFrame frame;
+	private JTextField textFieldUsername;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -15,6 +23,7 @@ public class LoginUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					LoginUI window = new LoginUI();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -36,8 +45,40 @@ public class LoginUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(500, 100, 350, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Welcome to eProctor");
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(100, 50, 150, 14);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(100, 100, 150, 14);
+		frame.getContentPane().add(lblPassword);
+		
+		JLabel lblDomain = new JLabel("Domain:");
+		lblDomain.setBounds(100, 150, 150, 14);
+		frame.getContentPane().add(lblDomain);
+		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setBounds(100, 70, 150, 20);
+		frame.getContentPane().add(textFieldUsername);
+		textFieldUsername.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(100, 120, 150, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(140, 192, 70, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		String[] domainStrings = { "Student", "Proctor" };
+		JComboBox cbxDomain = new JComboBox(domainStrings);
+		cbxDomain.setBounds(100, 170, 150, 20);
+		frame.getContentPane().add(cbxDomain);
 	}
-
 }
