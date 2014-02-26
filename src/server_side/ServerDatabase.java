fead16c32bbd1ab4bdb13dcbaaa7183485f193f6
cly753 <cly753@gmail.com>
2014-02-27 00:46:30 +0800

    database connection of verify user

---
+++ src/server_side/ServerDatabase.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import com.mongodb.*;
+
+public class ServerDatabase {
+    private static MongoClient mongoClient;
+    private static DB db;
+    private static DBCollection coll;
+
+    public ServerDatabase() {
+
+    }
+
+    public static boolean verifyUser(String userID, String md5, String domain) {
+        mongoClient = new MongoClient("localhost",27017);
+        db = mongoClient.getDB("testJavaMongo");
+        coll = db.getCollection("hhh");
+
+        try {
+            DBObject one = coll.findOne(new BasicDBObject("username", "gongy0000")
+                                            .append("md5", "30e04e5dfa8b1f8b3d56"));
+            mongoClient.close();
+
+//          System.out.println(one);
+//          System.out.println("passed");
+            
+            // ArrayList<String> queryResult = new ArrayList((BasicDBList) one.get("courseCode"));
+            // Iterator i = queryResult.iterator();
+            // while(i.hasNext())
+            //     System.out.println(i.next());
+
+            if (one)
+                return false;
+            return true;
+        } catch (NullPointerException e) {
+            mongoClient.close();
+            System.out.println("denied");
+            return false;
+        }
+    }
+}
