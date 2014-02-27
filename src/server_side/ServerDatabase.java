d07922d5b42e0aac242279d54b39e0e0a184bb78
cly753 <cly753@gmail.com>
2014-02-28 01:50:37 +0800

    hhh

--- src/server_side/ServerDatabase.java
+++ src/server_side/ServerDatabase.java
@@ -10,134 +10,131 @@ public class ServerDatabase {
 
     }
 
-    public static boolean verifyUser(String userID, String md5, String domain) {
-        try {
-            mongoClient = new MongoClient("localhost",27017);
-            db = mongoClient.getDB("testJavaMongo");
-            coll = db.getCollection("hhh");
-        } catch (UnknownHostException e) {
-            System.out.println("fail to connect to database");
-            return false;
-        }
+    // public static boolean verifyUser(String userID, String md5, String domain) {
+    //     try {
+    //         mongoClient = new MongoClient("localhost",27017);
+    //         db = mongoClient.getDB("testJavaMongo");
+    //         coll = db.getCollection("hhh");
+    //     } catch (UnknownHostException e) {
+    //         System.out.println("fail to connect to database");
+    //         return false;
+    //     }
 
-        try {
-            DBObject one = coll.findOne(new BasicDBObject("username", "gongy0000")
-                                            .append("md5", "30e04e5dfa8b1f8b3d56"));
-            mongoClient.close();
+    //     try {
+    //         DBObject one = coll.findOne(new BasicDBObject("username", "gongy0000")
+    //                                         .append("md5", "30e04e5dfa8b1f8b3d56"));
+    //         mongoClient.close();
 
-            // System.out.println(one);
-            // System.out.println("passed");
+    //         // System.out.println(one);
+    //         // System.out.println("passed");
             
-            // ArrayList<String> queryResult = new ArrayList((BasicDBList) one.get("courseCode"));
-            // Iterator i = queryResult.iterator();
-            // while(i.hasNext())
-            //     System.out.println(i.next());
+    //         // ArrayList<String> queryResult = new ArrayList((BasicDBList) one.get("courseCode"));
+    //         // Iterator i = queryResult.iterator();
+    //         // while(i.hasNext())
+    //         //     System.out.println(i.next());
 
-            if (!one)
-                return false;
-            return true;
-        } catch (NullPointerException e) {
-            mongoClient.close();
-            System.out.println("denied");
-            return false;
-        }
-    }
-
-    public static boolean updateExam(Exam newExam) {
-        try {
-            mongoClient = new MongoClient("localhost",27017);
-            db = mongoClient.getDB("testJavaMongo");
-            coll = db.getCollection("exam");
-        } catch (UnknownHostException e) {
-            System.out.println("fail to connect to database");
-            return false;
-        }
+    //         if (!one)
+    //             return false;
+    //         return true;
+    //     } catch (NullPointerException e) {
+    //         mongoClient.close();
+    //         System.out.println("denied");
+    //         return false;
+    //     }
+    // }
+    // public static boolean updateExam(Exam newExam) {
+    //     try {
+    //         mongoClient = new MongoClient("localhost",27017);
+    //         db = mongoClient.getDB("testJavaMongo");
+    //         coll = db.getCollection("exam");
+    //     } catch (UnknownHostException e) {
+    //         System.out.println("fail to connect to database");
+    //         return false;
+    //     }
         
 
-        try {
-            DBObject one = coll.update(new BasicDBObject("examId", newExam.examId));
-            mongoClient.close();
+    //     try {
+    //         DBObject one = coll.update(new BasicDBObject("examId", newExam.examId));
+    //         mongoClient.close();
 
-            coll.update(new BasicDBObject("examId", newExam.examId), 
-                        new BasicDBObject("$set", new BasicDBObject("courseCode", newExam.courseCode)
-                                                    .append("startTime", newExam.startTime)
-                                                    .append("endTime", newExam.endTime)
-                                                    .append("examAddress", newExam.examAddress)
-                                                    .append("proctorIdList", newExam.proctorIdList));
+    //         coll.update(new BasicDBObject("examId", newExam.examId), 
+    //                     new BasicDBObject("$set", new BasicDBObject("courseCode", newExam.courseCode)
+    //                                                 .append("startTime", newExam.startTime)
+    //                                                 .append("endTime", newExam.endTime)
+    //                                                 .append("examAddress", newExam.examAddress)
+    //                                                 .append("proctorIdList", newExam.proctorIdList));
 
-            mongoClient.close();
-            System.out.println("success");
-            return true;
-        } catch (NullPointerException e) {
-            mongoClient.close();
-            System.out.println("failed");
-            return false;
-        }
-    }
+    //         mongoClient.close();
+    //         System.out.println("success");
+    //         return true;
+    //     } catch (NullPointerException e) {
+    //         mongoClient.close();
+    //         System.out.println("failed");
+    //         return false;
+    //     }
+    // }
+    // public static ArrayList<Exam> getBookedExam(Student currentStudent) {
+    //     try {
+    //         mongoClient = new MongoClient("localhost",27017);
+    //         db = mongoClient.getDB("testJavaMongo");
+    //         coll = db.getCollection("exam");
+    //     } catch (UnknownHostException e) {
+    //         System.out.println("fail to connect to database");
+    //         return false;
+    //     }
 
-    public static ArrayList<Exam> getBookedExam(Student currentStudent) {
-        try {
-            mongoClient = new MongoClient("localhost",27017);
-            db = mongoClient.getDB("testJavaMongo");
-            coll = db.getCollection("exam");
-        } catch (UnknownHostException e) {
-            System.out.println("fail to connect to database");
-            return false;
-        }
+    //     try {
+    //         DBCursor cursor = coll.find(new BasicDBObject("data", new BasicDBObject("$elemMatch", new BasicDBObject("username", currentStudent.username))),
+    //                                 new BasicDBObject("startTime", 1)
+    //                                 .append("endTime", 1)
+    //                                 .append("courseCode", 1)
+    //                                 .append("examId", 1)
+    //                                 .append("examAddress", 1));
+    //         mongoClient.close();
 
-        try {
-            DBCursor cursor = coll.find(new BasicDBObject("data", new BasicDBObject("$elemMatch", new BasicDBObject("username", currentStudent.username))),
-                                    new BasicDBObject("startTime", 1)
-                                    .append("endTime", 1)
-                                    .append("courseCode", 1)
-                                    .append("examId", 1)
-                                    .append("examAddress", 1));
-            mongoClient.close();
+    //         ArrayList<Exam> bookedExam = new ArrayList();
+    //         DBObject exam;
+    //         while (cursor.hasNext()) {
+    //             exam = cursor.next();
+    //             bookedExam.add(new Exam(exam.get("examId"),
+    //                                     exam.get("courseCode"),
+    //                                     exam.get("startTime"),
+    //                                     exam.get("endTime"),
+    //                                     exam.get("examAddress")));
+    //         }
 
-            ArrayList<Exam> bookedExam = new ArrayList();
-            DBObject exam;
-            while (cursor.hasNext()) {
-                exam = cursor.next();
-                bookedExam.add(new Exam(exam.get("examId"),
-                                        exam.get("courseCode"),
-                                        exam.get("startTime"),
-                                        exam.get("endTime"),
-                                        exam.get("examAddress")));
-            }
+    //         System.out.println("success");
+    //         return bookedExam;
+    //     } catch (NullPointerException e) {
+    //         mongoClient.close();
+    //         System.out.println("failed");
+    //         return null;
+    //     }
+    // }
+    // public ArrayList<Course> getRegisteredCourse(Student currentStudent) {
+    //     try {
+    //         mongoClient = new MongoClient("localhost",27017);
+    //         db = mongoClient.getDB("testJavaMongo");
+    //         coll = db.getCollection("hhh");
+    //     } catch (UnknownHostException e) {
+    //         System.out.println("fail to connect to database");
+    //         return false;
+    //     }
 
-            System.out.println("success");
-            return bookedExam;
-        } catch (NullPointerException e) {
-            mongoClient.close();
-            System.out.println("failed");
-            return null;
-        }
-    }
+    //     try {
+    //         DBObject one = coll.findOne(new BasicDBObject("username", "gongy0000"),
+    //                                     new BasicDBObject("courseCode", 1));
+    //         mongoClient.close();
 
-    public ArrayList<Course> getRegisteredCourse(Student currentStudent) {
-        try {
-            mongoClient = new MongoClient("localhost",27017);
-            db = mongoClient.getDB("testJavaMongo");
-            coll = db.getCollection("hhh");
-        } catch (UnknownHostException e) {
-            System.out.println("fail to connect to database");
-            return false;
-        }
+    //         if (!one)
+    //             return null;
 
-        try {
-            DBObject one = coll.findOne(new BasicDBObject("username", "gongy0000"),
-                                        new BasicDBObject("courseCode", 1));
-            mongoClient.close();
-
-            if (!one)
-                return null;
-
-            ArrayList<String> registeredCourse = (ArrayList)(BasicDBList)one.get("courseCode");
-            return registeredCourse;
-        } catch (NullPointerException e) {
-            mongoClient.close();
-            System.out.println("denied");
-            return null;
-        }
-    }
+    //         ArrayList<String> registeredCourse = (ArrayList)(BasicDBList)one.get("courseCode");
+    //         return registeredCourse;
+    //     } catch (NullPointerException e) {
+    //         mongoClient.close();
+    //         System.out.println("denied");
+    //         return null;
+    //     }
+    // }
 }
