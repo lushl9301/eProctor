cc2edac8474734a6e886d8b6489892e50802eb27
Lu Shengliang <lushl9301@gmail.com>
2014-03-06 13:56:46 +0800

    add coordinator domian

---
+++ src/proctor_side/CoordinatorHomeController.java
@@ -0,0 +1,20 @@
+package proctor_side;
+
+import entity.Main;
+
+public class CoordinatorHomeController extends ProctorHomeController {
+    
+    public void setExamPaper(String link) {
+        //Main.client.updateData(examId, link)???
+        
+    }
+    
+    public void updateExamInfo(String examId) {
+        // TODO
+        // Open dialog/Frame
+        // fill in all previous info
+        // student modify
+        // update via client.java
+    }
+
+}
