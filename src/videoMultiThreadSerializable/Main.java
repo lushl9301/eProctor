a5487721b3fb04a65a5a6da43667d8929cbe3d36
Lu Shengliang <lushl9301@gmail.com>
2014-03-08 11:15:28 +0800

    use serialized byte[] write video via socket

---
+++ src/videoMultiThreadSerializable/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    static int port1 = 6000;
+    static int port2 = 6001;
+
+    public static void main(String[] args) throws Exception {
+        new GrabberShow(port2);
+    }
+}
---
+++ src/videoMultiThreadSerializable/Server.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Server {
+    private static int port1 = 6000;
+    private static int port2 = 6001;
+
+    public static void main(String[] args) throws IOException {
+        // new QueryServerThread(port1);
+        new VideoServerThread(port2);
+    }
+
+}
