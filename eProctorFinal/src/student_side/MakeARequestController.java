db58ecb7367e6e9dd43126be8006ebc30c3e6360
Lu Shengliang <lushl9301@gmail.com>
2014-03-12 16:00:30 +0800

    add makerequestUI

---
+++ eProctorFinal/src/student_side/MakeARequestController.java
@@ -0,0 +1,91 @@
+package student_side;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.QueryBuilder;
+
+import entity.Main;
+
+public class MakeARequestController {
+
+	private ObjectId record_id;
+	private DBObject obj, tObj;
+
+	public MakeARequestController() {
+		//Main.makeARequestUI.setVisible(true);
+		this.record_id = new ObjectId("531dc95e2ad939b68700001f");
+		QueryBuilder qb = new QueryBuilder();
+		qb.put("_id").is(record_id);
+		DBCursor cursor = Main.mongoHQ.record.find(qb.get());
+		if (cursor.hasNext()) {
+			obj = cursor.next();
+		}
+		if (obj == null) {
+			System.out.println("hehe");
+		}
+	}
+	
+	public void deleteRecord() {
+		// TODO Auto-generated method stub
+		Main.makeARequestUI.setVisible(false);
+	}
+
+	public void cancel() {
+		Main.makeARequestUI.setVisible(false);
+	}
+
+	public String getCourseCode() {
+		QueryBuilder qbQuery = new QueryBuilder();
+		qbQuery = new QueryBuilder();
+		qbQuery.put("_id").is(obj.get("course_id"));
+		tObj = Main.mongoHQ.course.findOne(qbQuery.get());
+		return (String) tObj.get("code");
+	}
+
+	public String getSessionId() {
+		return obj.get("session_id").toString();
+	}
+
+	public String getLocation() {
+		QueryBuilder qbQuery = new QueryBuilder();
+		qbQuery = new QueryBuilder();
+		qbQuery.put("_id").is(obj.get("session_id"));
+		tObj = Main.mongoHQ.session.findOne(qbQuery.get());
+		return (String) tObj.get("name");
+	}
+
+	public String getStartTime() {
+		QueryBuilder qbQuery = new QueryBuilder();
+		qbQuery = new QueryBuilder();
+		qbQuery.put("_id").is(obj.get("session_id"));
+		tObj = Main.mongoHQ.session.findOne(qbQuery.get());
+		Date startDate = new Date(Long.parseLong((String) tObj.get("start")));
+		SimpleDateFormat startFormat = new SimpleDateFormat ("dd.MM.yyyy E kk:mm");
+		return (String) startFormat.format(startDate);
+	}
+
+	public String getEndTime() {
+		QueryBuilder qbQuery = new QueryBuilder();
+		qbQuery = new QueryBuilder();
+		qbQuery.put("_id").is(obj.get("session_id"));
+		tObj = Main.mongoHQ.session.findOne(qbQuery.get());
+		Date endDate = new Date(Long.parseLong((String) tObj.get("end")));
+		SimpleDateFormat endFormat = new SimpleDateFormat ("dd.MM.yyyy E kk:mm");
+		return (String) endFormat.format(endDate);
+	}
+
+	public String getRecordId() {
+		return record_id.toString();
+	}
+
+	public String getProctorId() {
+		return (String) obj.get("proctor");
+	}
+
+}
---
+++ eProctorFinal/src/student_side/MakeARequestUI.java
@@ -0,0 +1,148 @@
+package student_side;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import entity.Main;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MakeARequestUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtCour;
+	private JTextField txtSession;
+	private JTextField txtCourseCode;
+	private JTextField textStartTime;
+	private JTextField textEndTime;
+	private JTextField txtLocation;
+	private JTextArea FYI;
+	private JLabel lblRecordid;
+	private JLabel lblProctorName;
+
+	/**
+	 * Create the frame.
+	 */
+	public MakeARequestUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Main.makeARequestController.deleteRecord();
+			}
+		});
+		btnDelete.setBounds(152, 232, 157, 25);
+		contentPane.add(btnDelete);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Main.makeARequestController.cancel();
+				setVisible(false);
+			}
+		});
+		btnCancel.setBounds(321, 232, 117, 25);
+		contentPane.add(btnCancel);
+		
+		txtCourseCode = new JTextField();
+		txtCourseCode.setEditable(false);
+		txtCourseCode.setText(Main.makeARequestController.getCourseCode());
+		txtCourseCode.setBounds(97, 50, 99, 22);
+		contentPane.add(txtCourseCode);
+		txtCourseCode.setColumns(10);
+		
+		txtCour = new JTextField();
+		txtCour.setEditable(false);
+		txtCour.setText(Main.makeARequestController.getCourseCode());
+		txtCour.setBounds(97, 85, 99, 22);
+		contentPane.add(txtCour);
+		txtCour.setColumns(10);
+		
+		txtSession = new JTextField();
+		txtSession.setEditable(false);
+		txtSession.setText(Main.makeARequestController.getSessionId());
+		txtSession.setBounds(97, 120, 99, 22);
+		contentPane.add(txtSession);
+		txtSession.setColumns(10);
+		
+		txtLocation = new JTextField();
+		txtLocation.setEditable(false);
+		txtLocation.setText(Main.makeARequestController.getLocation());
+		txtLocation.setBounds(288, 50, 150, 22);
+		contentPane.add(txtLocation);
+		txtLocation.setColumns(10);
+		
+		textStartTime = new JTextField();
+		textStartTime.setEditable(false);
+		textStartTime.setText(Main.makeARequestController.getStartTime());
+		textStartTime.setBounds(288, 85, 150, 22);
+		contentPane.add(textStartTime);
+		textStartTime.setColumns(10);
+		
+		textEndTime = new JTextField();
+		textEndTime.setEditable(false);
+		textEndTime.setText(Main.makeARequestController.getEndTime());
+		textEndTime.setBounds(288, 120, 150, 22);
+		contentPane.add(textEndTime);
+		textEndTime.setColumns(10);
+		
+		JLabel lblCourseCode = new JLabel("Code");
+		lblCourseCode.setBounds(30, 50, 105, 15);
+		contentPane.add(lblCourseCode);
+		
+		JLabel lblCourseName = new JLabel("Course");
+		lblCourseName.setBounds(30, 85, 105, 15);
+		contentPane.add(lblCourseName);
+		
+		JLabel lblSession = new JLabel("Session");
+		lblSession.setBounds(30, 120, 105, 15);
+		contentPane.add(lblSession);
+	
+		JLabel lblLocation = new JLabel("Location");
+		lblLocation.setBounds(208, 50, 70, 15);
+		contentPane.add(lblLocation);
+		
+		JLabel labelStartTime = new JLabel("StartTime");
+		labelStartTime.setBounds(208, 85, 70, 15);
+		contentPane.add(labelStartTime);
+		
+		JLabel lblEndTime = new JLabel("End Time");
+		lblEndTime.setBounds(208, 120, 70, 15);
+		contentPane.add(lblEndTime);	
+		
+		FYI = new JTextArea();
+		FYI.setLineWrap(true);
+		FYI.setEditable(false);
+		FYI.setText("*If any updates of booked exam session is needed,please delete current record before making new booking.");
+		FYI.setBounds(68, 160, 370, 60);
+		contentPane.add(FYI);
+		
+		lblRecordid = new JLabel("Record: " + Main.makeARequestController.getRecordId());
+		lblRecordid.setBounds(28, 12, 117, 15);
+		contentPane.add(lblRecordid);
+		
+		lblProctorName = new JLabel("Proctor: " + Main.makeARequestController.getProctorId());
+		lblProctorName.setBounds(150, 12, 117, 15);
+		contentPane.add(lblProctorName);
+	}
+}
