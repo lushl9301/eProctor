d864c803984a1d802b6f5db358fdbcb8770a47e4
Lu Shengliang <lushl9301@gmail.com>
2014-04-10 14:59:42 +0800

    update RecordObject.java

--- videoMultiReceive/RecordObject.java
+++ videoMultiReceive/RecordObject.java
@@ -6,18 +6,25 @@ public class RecordObject implements Serializable {
 
     protected static final long serialVersionUID = 1123L;
     public String userId;
-    public byte[] imageBytes;
+    public byte[] cameraBytes;
+    public byte[] screenBytes
 
-    public RecordObject(String userId, byte[] imageBytes) {
+    public RecordObject(String userId, byte[] cameraBytes, byte[] screenBytes) {
         this.userId = userId;
-        this.imageBytes = imageBytes;
+        this.cameraByte = cameraBytes;
+        this.screenBytes = screenBytes
     }
 
     public String getUserId() {
         return this.userId;
     }
 
-    public byte[] getImageBytes() {
-        return this.imageBytes;
+    public byte[] getCameraBytes() {
+        return this.cameraByte;
     }
+    
+    public byte[] getScreenBytes() {
+    	return this.screenBytes;
+    }
+    
 }
